38613efeaecf962f608811b5ad575f57b5a6c701
sachin sharma <sachin2252sharma@gmail.com>
2022-01-13 11:15:49 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, View, SafeAreaView } from "react-native";
+import CardDetails from "./Components/CardDetails";
+import Topbar from "./Components/UI/Header/Topbar";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Topbar />
+      <View style={styles.container}>
+        <CardDetails />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: "25%",
+  },
+  main: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20,
+    backgroundColor: "#ffff",
   },
 });
